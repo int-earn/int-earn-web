b8b4a6183d8929d807820257e20270ccd6fed134
syj0396 <syj0396@naver.com>
2024-01-12 11:39:23 +0900

    feat: signup ui

--- frontend/src/pages/join.js
+++ frontend/src/pages/join.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { TextField, Typography, Grid }from '@mui/material'
+import { TextField, Typography, Grid, OutlinedInput, Button }from '@mui/material'
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
     nickname: "",
@@ -10,14 +11,15 @@ const initialState = {
 }
 
 export default function Join() {
+    const navigate = useNavigate();
     const [{ nickname, username, password, rePassword }, setState] = useState(initialState);
     const handleChange = (e) => {
         const {name, value} = e.target;
-        console.log(name, value)
         setState((prevState) => ({...prevState, [name]: value}))
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log("t")
         const data = {
             nickname: nickname,
             username: username,
@@ -26,50 +28,130 @@ export default function Join() {
         }
         const response = await axios.post('http://localhost:8080/api/user/save', data)
         console.log(response);
+        if (response.status === 201) {
+            navigate('/')
+        } else {
+            
+        }
     }
     return (
-        <div style={{marginTop: '100px'}}>
-        <div id="contact" style={{margin: '0 auto', width: '100%', height: '90%', position: 'relative'}}>
-            <div style={{width: '500px', height: '450px', position: 'absolute', top:'50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        <div style={{marginTop: '60px'}}>
+        <div id="contact" style={{margin: '0 auto', width: '100%', height: '100%', position: 'relative', paddingTop: '0'}}>
+            <div style={{width: '500px', height: '450px', position: 'absolute', top:'43%', left: '50%', transform: 'translate(-50%, -50%)'}}>
             <div className='section-title'>
                 <h2>Sign up</h2>
             </div>
-            <form onSubmit={handleSubmit}>
-                <Typography>닉네임</Typography>
-                {/* <div><label style={{display: 'block'}}>닉네임</label></div>
-                <input 
-                    type="text"
-                    name="nickname" 
-                    value={nickname} 
-                    className="form-control"
-                    onChange={handleChange} 
-                    style={{color: 'black', width: '300px', height: '50px', borderRadius: '10px', marginBottom: '12px'}}
+            <form onSubmit={handleSubmit} style={{display: 'column', justifyContent: 'center'}}>
+                <Typography style={{margin: '0 0 5px 5px', fontWeight: 'bold'}}>닉네임</Typography>
+                <OutlinedInput 
+                    //color="none"
+                    placeholder="닉네임"
+                    name="nickname"
+                    value={nickname}
+                    onChange={handleChange}
+                    sx={{
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none'
+                        },
+                        '& input': {
+                            fontSize: '13px',
+                        },
+                        width: '300px',
+                        height: '50px',
+                        //border: validUsername == false ? '1px solid #FF3B3B' : '1px solid #E2E2E2',
+                        border: '1px solid #E2E2E2',
+                        borderRadius: '8px',
+                        backgroundColor: 'white',
+                        outline: 'none',
+                        appearance: 'none',
+                        fontSize: '16px',
+                    }}
                 />
-                <input 
-                    type="text"
-                    name="username" 
-                    value={username} 
-                    className="form-control"
-                    onChange={handleChange} 
-                    style={{color: 'black', width: '300px', height: '50px', borderRadius: '10px', marginBottom: '12px'}}
+
+            <Typography style={{margin: '15px 0 5px 5px', fontWeight: 'bold'}}>아이디</Typography>
+                <OutlinedInput 
+                    //color="none"
+                    placeholder="아이디"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                    sx={{
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none'
+                        },
+                        '& input': {
+                            fontSize: '13px',
+                        },
+                        width: '300px',
+                        height: '50px',
+                        //border: validUsername == false ? '1px solid #FF3B3B' : '1px solid #E2E2E2',
+                        border: '1px solid #E2E2E2',
+                        borderRadius: '8px',
+                        backgroundColor: 'white',
+                        outline: 'none',
+                        appearance: 'none',
+                        fontSize: '16px',
+                    }}
                 />
-                <input 
-                    type="password"
-                    name="password" 
-                    value={password} 
-                    className="form-control"
-                    onChange={handleChange} 
-                    style={{color: 'black', width: '300px', height: '50px', borderRadius: '10px', marginBottom: '12px'}}
+
+            <Typography style={{margin: '15px 0 5px 5px', fontWeight: 'bold'}}>비밀번호</Typography>
+                <OutlinedInput 
+                    //color="none"
+                    placeholder="비밀번호"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                    sx={{
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none'
+                        },
+                        '& input': {
+                            fontSize: '13px',
+                        },
+                        width: '300px',
+                        height: '50px',
+                        //border: validUsername == false ? '1px solid #FF3B3B' : '1px solid #E2E2E2',
+                        border: '1px solid #E2E2E2',
+                        borderRadius: '8px',
+                        backgroundColor: 'white',
+                        outline: 'none',
+                        appearance: 'none',
+                        fontSize: '16px',
+                    }}
                 />
-                <input 
-                    type="password"
-                    name="rePassword" 
-                    value={rePassword} 
-                    className="form-control"
-                    onChange={handleChange} 
-                    style={{color: 'black', width: '300px', height: '50px', borderRadius: '10px', marginBottom: '12px'}}
+
+            <Typography style={{margin: '15px 0 5px 5px', fontWeight: 'bold'}}>비밀번호 확인</Typography>
+                <OutlinedInput 
+                    //color="none"
+                    placeholder="비밀번호 재입력"
+                    name="rePassword"
+                    value={rePassword}
+                    onChange={handleChange}
+                    sx={{
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none'
+                        },
+                        '& input': {
+                            fontSize: '13px',
+                        },
+                        width: '300px',
+                        height: '50px',
+                        //border: validUsername == false ? '1px solid #FF3B3B' : '1px solid #E2E2E2',
+                        border: '1px solid #E2E2E2',
+                        borderRadius: '8px',
+                        backgroundColor: 'white',
+                        outline: 'none',
+                        appearance: 'none',
+                        fontSize: '16px',
+                    }}
                 />
-                <button type="submit">submit</button> */}
+
+            <Button 
+                onClick={handleSubmit}
+                className="btn btn-custom btn-lg" 
+                style={{margin: '45px 0', width: '300px', fontSize: '14px', border: '1px solid white', borderRadius: '8px', height: '56px', boxShadow: 'none'}}>
+                회원가입
+            </Button>
             </form>
             </div>
         </div>
