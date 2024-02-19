dadb8d9038bc3a974d8572ac691bcd5790ffd764
nicedavid98 <nicedavid98@daum.net>
2024-02-19 16:06:51 +0900

    BoardRepository, CommentRepository 구현완료.

---
+++ backend/src/main/java/com/intearn/backend/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.intearn.backend.repository;
+
+import com.intearn.backend.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
